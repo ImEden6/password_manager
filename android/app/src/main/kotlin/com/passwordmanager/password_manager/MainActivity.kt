43abe83b706d2c30f3c2bbabdd084a7d319c1a0d
ImEden6 <mervyn.h.mh@gmail.com>
2026-03-26 10:32:04 +0800

    has blur now

--- android/app/src/main/kotlin/com/passwordmanager/password_manager/MainActivity.kt
+++ android/app/src/main/kotlin/com/passwordmanager/password_manager/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.passwordmanager.password_manager
 
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterFragmentActivity()
+class MainActivity : FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        // This flag blurs the app in the recent apps switcher and blocks screenshots
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+    }
+}
 
